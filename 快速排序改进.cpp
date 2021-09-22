b891935d50157cd4d592b1fb80b85ae45c7c357d
hyl-code <2839797603@qq.com>
2021-09-22 10:42:10 +0800

    order

---
+++ 快速排序改进.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <iomanip>
+#define NUM 10
+using namespace std;
+
+typedef struct{
+	int *elem;
+	int length;
+}SqList;
+
+void QuickSort(SqList & L, int low, int high){
+	// i指向小于枢轴的最后一个元素 , k指向大于枢轴的最后一个元素
+	// j指向当前待考察的元素  
+	int i = low - 1;
+	int j = low;
+	int k = high + 1;
+	int piv, temp;
+	
+	if(low >= high)
+		return;
+	piv = L.elem[low];
+	while(j < k){
+		if(L.elem[j] < piv){
+			i++;
+			temp = L.elem[j];
+			L.elem[j] = L.elem[i];
+			L.elem[i] = temp;
+			j++;
+		}else if(L.elem[j] > piv){
+			k--;
+			temp = L.elem[j];
+			L.elem[j] = L.elem[k];
+			L.elem[k] = temp; 
+		}else
+			j++;
+	}//划分小于枢轴、等于枢轴、大于枢轴 
+	QuickSort(L, low, i);
+	QuickSort(L, k, high);
+}
+
+int main(void){
+	int a[NUM] = {4, 4, 4, 3, 3, 1, 1, 2, 2, 4};
+	SqList L;
+	int i;
+	L.elem = a;
+	L.length = NUM;
+	QuickSort(L, 0, L.length - 1);
+	for(i = 0; i < L.length; i++){
+		cout <<setw(2)<<L.elem[i];
+	}
+	
+}
